dd99b440f7bddba7c62ad3f80beef6f0c0c13e75
anton.tsygankov <84995522+monimah@users.noreply.github.com>
2022-07-22 02:53:07 +0300

    .

--- backend/messages/ru/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'Upload image' => 'Загрузите фото',
-    'Image key not found' => 'Ключ "img" не найдено',
-    'Аccount is blocked' => 'Аккаунт заблокирован',
-    'Username or password incorrect' => 'Логин или пароль неверен',
-];
